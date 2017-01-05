3facbe049ac062bfb46b646de7a2e9c8ab6916c8
shanky25051987 <shanky25051987@gmail.com>
2017-01-06 01:49:16 +0530

    app_spec.js
    test cases with the real data

--- Unit/jasmine-standalone-2.5.2/spec/app_spec.js
+++ Unit/jasmine-standalone-2.5.2/spec/app_spec.js
@@ -1,7 +1,7 @@
  /* Tests */
       describe('http tests', function () {
         
-        beforeEach(module('SampleApp'));
+        beforeEach(module('petsApp'));
 
         var $controller;
         var $httpBackend;
@@ -18,23 +18,68 @@
             $httpBackend = _$httpBackend_;
 
             // Note that this HTTP backend is ngMockE2E's, and will make a real HTTP request
-            $httpBackend.whenGET('http://www.omdbapi.com/?s=terminator').passThrough();
+            $httpBackend.whenJSONP('https://agl-developer-test.azurewebsites.net/people.json?callback=JSON_CALLBACK&format=jsonp').passThrough();
+			
           }));
 
-          it('should load default movies (with real http request)', function (done) {
-            var sampleController = $controller('SampleController', { $scope: $scope });
+          it('should load default pet data (with real http request)', inject(function($http) {
+            var sampleController = $controller('appCntrl', { $scope: $scope });
+			
+			var url = "https://agl-developer-test.azurewebsites.net/people.json";
+			$http({
+            	method: 'jsonp',
+            	url: url,
+            	params: {
+            		format: 'jsonp',
+            		callback: 'JSON_CALLBACK'
+            	}
+            }).success(function (data) {
+					$scope.response = data;
+					
+			});
+				
+				//expect(data).not.toEqual([]);
+				$httpBackend.expectJSONP('https://agl-developer-test.azurewebsites.net/people.json?callback=JSON_CALLBACK&format=jsonp').respond(200);
+				//$httpBackend.flush();
+				expect($httpBackend.flush).not.toThrow();
+				expect($scope.response).not.toEqual([]);
+				
+			
+            /*setTimeout(function() {
 
-            setTimeout(function() {
+              console.log('Look, pet data:');
+              console.log($scope.data);
 
-              console.log('Look, real data:');
-              console.log($scope.movies);
+              
+              
+            }, 1000);*/
 
-              expect($scope.movies).not.toEqual([]);
-              done();
-            }, 1000);
-
-          });
+          }));
 
         });
 
       });  
+	  
+describe('The test filter', function () { 
+
+  var $filter;
+
+  beforeEach(function () {
+    module('petsApp');
+
+    inject(function (_$filter_) {
+      $filter = _$filter_;
+    });
+  });
+
+  it('should give cat type pet name', function () {
+    // Arrange.
+    var foo = {name: "Garfield", type: "Cat"}, result;
+
+    // Act.
+    result = $filter('animalType')(foo);
+
+    // Assert.
+    expect(result).toEqual(['Garfield']);
+  });
+});
